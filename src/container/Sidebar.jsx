eb996bc01a0f0bd0338f64f3b730762ec4e79c9f
Shaik Jasmin <jasminshaik@msstechno.com>
2026-06-04 09:56:40 +0530

    sidebar open

--- src/container/Sidebar.jsx
+++ src/container/Sidebar.jsx
@@ -23,7 +23,7 @@ function Sidebar({ children }) {
         ?.trim();
     console.log("ROLE:", role);
     const employee_id = localStorage.getItem("employee_id");
-    const [openSidebar, setOpenSidebar] = useState(false);
+    const [openSidebar, setOpenSidebar] = useState(true);
 
     const handleLogout = async () => {
         try {
